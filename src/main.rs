cbfe3e6f2dc2d164e61d91f893402a65e2b2f427
Ilia <istudyatuni@gmail.com>
2022-08-05 10:59:51 +0300

    refactor: move main() to the top

--- src/main.rs
+++ src/main.rs
@@ -12,44 +12,6 @@ mod model;
 const SLEEP_DURATION: Duration = Duration::from_secs(1);
 const TEN_MIN: Duration = Duration::from_secs(10 * 60);
 
-fn init_db() -> Result<DB, CrateError> {
-    #[cfg(feature = "prod")]
-    let db_path: String = if let Some(home_dir) = dirs::home_dir() {
-        format!("{}/itmo.db", home_dir.display())
-    } else {
-        eprintln!("no $HOME for storing database file, using /");
-        "/itmo.db".to_string()
-    };
-    #[cfg(not(feature = "prod"))]
-    let db_path = "test.db".to_string();
-
-    DB::new(&db_path)
-}
-
-async fn check_rating_updates(db: &DB) -> Result<(), CrateError> {
-    // select registered watchers from 'results'
-    match db.select_all_competitions() {
-        Ok(competitions) => {
-            for c in competitions {
-                if let Some(case_number) = c.competition.case_number {
-                    handle_competition(
-                        db,
-                        &c.tg_chat_id,
-                        &c.degree,
-                        &case_number,
-                        &c.program_id,
-                        false,
-                    )
-                    .await?
-                }
-            }
-        }
-        Err(e) => eprintln!("Error selecting all competitions: {e}"),
-    }
-
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() -> Result<(), CrateError> {
     let db = match init_db() {
@@ -83,10 +45,48 @@ async fn main() -> Result<(), CrateError> {
     }
 }
 
+fn init_db() -> Result<DB, CrateError> {
+    #[cfg(feature = "prod")]
+    let db_path: String = if let Some(home_dir) = dirs::home_dir() {
+        format!("{}/itmo.db", home_dir.display())
+    } else {
+        eprintln!("no $HOME for storing database file, using /");
+        "/itmo.db".to_string()
+    };
+    #[cfg(not(feature = "prod"))]
+    let db_path = "test.db".to_string();
+
+    DB::new(&db_path)
+}
+
 async fn check_rating_updates_wrapper(db: &DB) -> Result<Instant, CrateError> {
     db.purge_cache()?;
     if let Err(e) = check_rating_updates(db).await {
         eprintln!("Error checking rating updates: {e}")
     }
     Ok(Instant::now())
+}
+
+async fn check_rating_updates(db: &DB) -> Result<(), CrateError> {
+    // select registered watchers from 'results'
+    match db.select_all_competitions() {
+        Ok(competitions) => {
+            for c in competitions {
+                if let Some(case_number) = c.competition.case_number {
+                    handle_competition(
+                        db,
+                        &c.tg_chat_id,
+                        &c.degree,
+                        &case_number,
+                        &c.program_id,
+                        false,
+                    )
+                    .await?
+                }
+            }
+        }
+        Err(e) => eprintln!("Error selecting all competitions: {e}"),
+    }
+
+    Ok(())
 }
